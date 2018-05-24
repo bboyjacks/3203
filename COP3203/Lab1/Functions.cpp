1e6c60c484979484f0dbcfcdd6f87ec8156973ff
Lester <lgdelacruz92@gmail.com>
2018-05-23 19:52:20 -0700

    Added Functions Methods

--- COP3203/Lab1/Functions.cpp
+++ COP3203/Lab1/Functions.cpp
@@ -1,2 +1,36 @@
 #include "Functions.h"
 
+bool Functions::isValid(int number)
+{
+  return false;
+}
+
+int Functions::sumOfDoubleEvenPlace(int number)
+{
+  return 0;
+}
+
+int Functions::getDigit(int number)
+{
+  return 0;
+}
+
+int Functions::sumOfOddPlace(int number)
+{
+  return 0;
+}
+
+bool Functions::prefixMatched(int number, int digit)
+{
+  return false;
+}
+
+int Functions::getSize(int number)
+{
+  return 0;
+}
+
+int Functions::getPrefix(int number, int numDigits)
+{
+  return 0;
+}
--- COP3203/Lab1/Functions.h
+++ COP3203/Lab1/Functions.h
@@ -1,5 +1,16 @@
 #pragma once
 namespace Functions
 {
+  bool isValid(int number);
+
+  int sumOfDoubleEvenPlace(int number);
+
+  int getDigit(int number);
+
+  int sumOfOddPlace(int number);
+
+  bool prefixMatched(int number, int digit);
+
+  int getSize(int number);
 };
 
