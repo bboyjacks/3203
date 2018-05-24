339f557cf9b0d6aed47df52e10a8dc8e7928ef23
Lester <lgdelacruz92@gmail.com>
2018-05-23 17:57:25 -0700

    Working user input

--- COP3203/Lab1/Source/main.cpp
+++ COP3203/Lab1/Source/main.cpp
@@ -1,8 +1,64 @@
 #include <iostream>
+#include <string>
+
+void execQuadraticRoot()
+{
+  std::string a;
+  std::string b;
+  std::string c;
+  std::cout << "Enter a, b, c: " << std::endl;
+  std::cin >> a >> b >> c;
+  std::cout << a << " " << b << " " << c << std::endl;
+}
+
+void execGradeCalculator()
+{
+  std::cout << "Grade Calculator" << std::endl;
+}
+
+void execCreditCardValidator()
+{
+  std::cout << "Credit card validator" << std::endl;
+}
 
 int main()
 {
-	std::cout << "Hello world!" << std::endl;
-	std::getchar();
-	return 0;
+  bool validLastInput{ true };
+  bool runChar{ true };
+  std::string input;
+  while (true)
+  {
+    if (validLastInput)
+    {
+      std::cout << "1. Quadratic Root" << std::endl;
+      std::cout << "2. Grade Calculator" << std::endl;
+      std::cout << "3. Credit Card Validator" << std::endl;
+      std::cout << "Enter a number: " << std::endl;
+    }
+    std::cin >> input;
+    if (!input.empty())
+    {
+      const int choice = std::stoi(input);
+      if (choice == 1)
+      {
+        validLastInput = true;
+        execQuadraticRoot();
+      }
+      else if (choice == 2)
+      {
+        validLastInput = true;
+        execGradeCalculator();
+      }
+      else if (choice == 3)
+      {
+        validLastInput = true;
+        execCreditCardValidator();
+      }
+      else
+      {
+        validLastInput = false;
+      }
+    }
+  }
+  return 0;
 }
