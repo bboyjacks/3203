ec786a9bef0b392a92a420112076ff545f9eeb3f
Lester <lgdelacruz92@gmail.com>
2018-05-23 19:44:15 -0700

    Completed Grade Calculator

--- COP3203/Lab1/Source/main.cpp
+++ COP3203/Lab1/Source/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 void execQuadraticRoot()
 {
@@ -35,7 +36,49 @@ void execQuadraticRoot()
 
 void execGradeCalculator()
 {
-  std::cout << "Grade Calculator" << std::endl;
+  std::string input_str;
+  std::cout << "Enter the number of students: ";
+  std::cin >> input_str;
+  const unsigned int input_int = std::stoi(input_str);
+  std::vector<int> scores(input_int);
+  std::vector<char> grades(5);
+
+  int best_score = -1;
+
+  std::cout << "Enter " << input_int << " scores: ";
+  for (unsigned int i = 0; i < input_int; i++)
+  {
+    std::string grade_str;
+    std::cin >> grade_str;
+    const int grade_int = std::stoi(grade_str);
+    scores[i] = grade_int;
+    if (best_score < scores[i])
+      best_score = scores[i];
+  }
+
+  for (unsigned int i = 0; i < input_int; i++)
+  {
+    if (scores[i] >= best_score - 10)
+    {
+      grades[i] = 'A';
+    }
+    else if (scores[i] >= best_score - 20)
+    {
+      grades[i] = 'B';
+    }
+    else if (scores[i] >= best_score - 30)
+    {
+      grades[i] = 'C';
+    }
+    else if (scores[i] >= best_score - 40)
+    {
+      grades[i] = 'D';
+    }
+    else
+      grades[i] = 'F';
+
+    std::cout << "Student " << i + 1 << " - Score: " << scores[i] << ", Letter: " << grades[i] << std::endl;;
+  }
 }
 
 void execCreditCardValidator()
