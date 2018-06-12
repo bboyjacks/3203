18e9f319ea566496c6dd45c080e3692d380b0093
Lester <lgdelacruz92@gmail.com>
2018-06-12 10:29:58 -0700

    Started Lab4

--- COP3203/Lab4/main.cpp
+++ COP3203/Lab4/main.cpp
@@ -1,32 +1,69 @@
 #include <iostream>
+#include "stdio.h"
 
-void PartOne()
+using namespace std;
+
+double max_heightd(double v_i, double g)
 {
-  const float v_final_f = 0.0f;
-  const float v_initial_f = 30.0f;
-  const float change_in_velocity_f = -9.8f;
-  const float time_f = (v_final_f - v_initial_f) / change_in_velocity_f;
-  const float y_max_f = v_initial_f * time_f + (0.5f) * change_in_velocity_f * time_f * time_f;
-  std::cout << "Max height (float): " << y_max_f << std::endl;
+  // v_f - v_i = a * t
+  // solve for t
 
-  const double y_max_d = (0.5 * 30 * 30)/9.8;
-  std::cout << "Max height (double): " << y_max_d << std::endl;
+  // h = v_i * t + 1/2 * a * t^2
+  // return h as the max height
+  return 0;
+}
 
-  const float dt = 0.01f;
-  float v_numerical_f = v_initial_f;
-  float y_max_numerical_f = 0.0f;
+float max_heightf(float v_i, float g)
+{
+  // v_f - v_i = a * t
+  // solve for t
 
-  while (true)
-  {
+  // h = v_i * t + 1/2 * a * t^2
+  // return h as the max height
+  return 0;
+}
 
-  }
+float simulatef(float v_i, float g, float dt)
+{
+  //declare float versions to update the velocity and height
+  //loop code
+  //condition to break the loop
+  //update velocity
+  //update height
+  //return the height
+  return 0;
+}
 
-  std::cout << y_max_numerical_f << std::endl;
+double simulated(double v_i, double g, double dt)
+{
+  //declare double versions to update the velocity and height
+  //loop code
+  //condition to break the loop
+  //update velocity
+  //update height
+  //return the height
+  return 0;
 }
 
 int main()
 {
-  PartOne();
-  std::getchar();
+  double gd = -9.81; // gravitational acceleration
+  double v_id = 500.0; // initial velocity
+  double dtd = 0.01; // delta t, change in time, time resolution
+                     // analytic (double) height (m) = 12742.1
+                     // analytic (float) height (m) = 12742.1
+                     // simulation (double) height (m) = 12739.6
+                     // simulation (float) height (m) = 12738.3
+
+                     // cin >> gd;
+                     // cin >> v_id;
+                     // cin >> dtd;
+  float gf = gd;
+  float v_if = v_id;
+  float dtf = dtd;
+  cout << "analytic (double) height (m) = " << max_heightd(v_id, gd) << endl;
+  cout << "analytic (float) height (m) = " << max_heightf(v_if, gf) << endl;
+  cout << "simulation (double) height (m) = " << simulated(v_id, gd, dtd) << endl;
+  cout << "simulation (float) height (m) = " << simulatef(v_if, gf, dtf) << endl;
   return 0;
 }
